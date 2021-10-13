90e8d0fed6696b1f8b21acde9e22af74df01a127
ethanphuong <ephuong@gmail.com>
2021-10-13 13:01:29 -0700

    Working on part 3

--- source/main.c
+++ source/main.c
@@ -18,150 +18,151 @@ int main(void) {
     DDRB = 0xFF; PORTB = 0x00;
     /* Insert DDR and PORT initializations */
     /* Insert your solution below */
-
-    while (1) 
-    {
+    while (1) {
       Tick_LoHi();
     }
     return 1;
 }
 
-enum SM1_STATES {SM1_SMStart, SM1_zeroLight, SM1_oneLight, SM1_twoLight, SM1_threeLight, SM1_fourLight, SM1_fiveLight, SM1_sixLight, SM1_Release1, SM1_Release2, SM1_Release3, SM1_Release4, SM1_Release5} SM1_STATE;
+enum SM1_STATES{SM1_SMStart, SM1_zeroLight, SM1_oneLight, SM1_twoLight, SM1_threeLight, SM1_fourLight, SM1_fiveLight, SM1_sixLight, SM1_ReleaseOne, SM1_ReleaseTwo, SM1_ReleaseThree, SM1_ReleaseFour, SM1_ReleaseFive} SM1_STATE;
 void Tick_LoHi() {
   switch(SM1_STATE) { 
     case SM1_SMStart:
-      if (1) {
-	 SM1_STATE = SM1_zeroLight;
-      }
-      break;
+    SM1_STATE = SM1_zeroLight;
+    break;
     case SM1_zeroLight:
-      if (~PINA & 0x01) {
-      	SM1_STATE = SM1_oneLight;
-      } 
-      else {
-      	SM1_STATE = SM1_zeroLight;
-      }
-      break;
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_oneLight;
+    } 
+    else {
+      SM1_STATE = SM1_zeroLight;
+    }
+    break;
     case SM1_oneLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_Release1;
-      } 
-      else {
-        SM1_STATE = SM1_oneLight;
-      }
-      break;
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_ReleaseOne;
+    } 
+    else {
+      SM1_STATE = SM1_oneLight;
+    }
+    break;
     case SM1_twoLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_Release2;
-      } 
-      else {
-      	SM1_STATE = SM1_twoLight;
-      }
-      break;
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_ReleaseTwo;
+    } 
+    else {
+      SM1_STATE = SM1_twoLight;
+    }
+    break;
     case SM1_threeLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_Release3;
-      } 
-      else {
-        SM1_STATE = SM1_threeLight;
-      }
-      break;
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_ReleaseThree;
+    } 
+    else {
+      SM1_STATE = SM1_threeLight;
+    }
+    break;
     case SM1_fourLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_Release4;
-      } else {
-        SM1_STATE = SM1_fourLight;
-      }
-      break;
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_ReleaseFour;
+    } 
+    else {
+      SM1_STATE = SM1_fourLight;
+    }
+    break;
     case SM1_fiveLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_Release5;
-      } else {
-        SM1_STATE = SM1_fiveLight;
-      }
-      break;
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_ReleaseFive;
+    } 
+    else {
+      SM1_STATE = SM1_fiveLight;
+    }
+    break;
     case SM1_sixLight:
-      if (!(~PINA & 0x01)) {
-        SM1_STATE = SM1_zeroLight;
-      } 
-      else {
-        SM1_STATE = SM1_sixLight;
-      }
-      break;
-    case SM1_Release1:
-      if (~PINA & 0x01) {
-        SM1_STATE = SM1_twoLight;
-      } 
-      else {
-      	SM1_STATE = SM1_Release1;
-      }
-      break;
-    case SM1_Release2:
-      if (~PINA & 0x01) {
-        SM1_STATE = SM1_threeLight;
-      } 
-      else {
-        SM1_STATE = SM1_Release2;
-      }
-      break;
-    case SM1_Release3:
-      if (~PINA & 0x01) {
-        SM1_STATE = SM1_fourLight;
-      } 
-      else {
-        SM1_STATE = SM1_Release3;
-      }
-      break;
-    case SM1_Release4:
-      if (~PINA & 0x01) {
-        SM1_STATE = SM1_fiveLight;
-      } else {
-        SM1_STATE = SM1_Release4;
-      }
-      break;
-    case SM1_Release5:
-      if (~PINA & 0x01) {
-        SM1_STATE = SM1_sixLight;
-      } 
-      else {
-        SM1_STATE = SM1_Release5;
-      }
-      break;
-  }
-  switch (SM1_SMStart) 
-  {
+    if ((~PINA & 0x01) != 0x01) {
+      SM1_STATE = SM1_zeroLight;
+    } 
+    else {
+      SM1_STATE = SM1_sixLight;
+    }
+    break;
+    case SM1_ReleaseOne:
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_twoLight;
+    } 
+    else {
+      SM1_STATE = SM1_ReleaseOne;
+    }
+    break;
+    case SM1_ReleaseTwo:
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_threeLight;
+    } 
+    else {
+      SM1_STATE = SM1_ReleaseTwo;
+    }
+    break;
+    case SM1_ReleaseThree:
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_fourLight;
+    } 
+    else {
+      SM1_STATE = SM1_ReleaseThree;
+    }
+    break;
+    case SM1_ReleaseFour:
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_fiveLight;
+    } 
+    else {
+      SM1_STATE = SM1_ReleaseFour;
+    }
+    break;
+    case SM1_ReleaseFive:
+    if ((~PINA & 0x01) == 0x01) {
+      SM1_STATE = SM1_sixLight;
+    } 
+    else {
+      SM1_STATE = SM1_ReleaseFive;
+    }
+    break;
+    default:
+      SM1_STATE = SM1_zeroLight;
+    break;
+}
+  switch (SM1_STATE) { 
     case SM1_SMStart:
       break;
     case SM1_zeroLight:
+      PORTB = 0x00;
       break;
     case SM1_oneLight:
-      break;
-    case SM1_twoLight:
-      break;
-    case SM1_threeLight:
-      break;
-    case SM1_fourLight:
-      break;
-    case SM1_fiveLight:
-      break;
-    case SM1_sixLight:
-      break;
-    case SM1_Release1:
       PORTB = 0x01;
       break;
-    case SM1_Release2:
+    case SM1_twoLight:
       PORTB = 0x03;
-      break;
-    case SM1_Release3:
+    case SM1_threeLight:
       PORTB = 0x07;
       break;
-    case SM1_Release4:
+    case SM1_fourLight:
       PORTB = 0x0F;
       break;
-    case SM1_Release5:
+    case SM1_fiveLight:
+      PORTB = 0x1F;
+      break;
+    case SM1_sixLight:
       PORTB = 0x3F;
       break;
+    case SM1_ReleaseOne:
+      break;
+    case SM1_ReleaseTwo:
+      break;
+    case SM1_ReleaseThree:
+      break;
+    case SM1_ReleaseFour:
+      break; 
+    case SM1_ReleaseFive:
+      break;  
   }
 }
-
 
