dc4b1f845c78435e2b21e20387bcc0d4951cd741
ethanphuong <ephuong@gmail.com>
2021-10-13 01:13:02 -0700

    Working on part 3

--- source/main.c
+++ source/main.c
@@ -12,9 +12,10 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; 
-    	DDRC = 0xFF; PORTC = 0x07;
+    	DDRC = 0xFF; PORTB = 0x00;
 	while(1)
 	{
 		Tick_LoHi();
@@ -22,103 +23,118 @@ int main(void) {
 	return 1;
 }
 
-enum SM1_STATES { SM1_SMStart, SM1_nonePressed, SM1_first, SM1_second, SM1_bothPress, SM1_firstPress, SM1_secondPress} SM1_STATE;
+enum SM1_STATES { SM1_SMStart, SM1_oneLight, SM1_twoLight, SM1_threeLight, SM1_fourLight, SM1_fiveLight, SM1_sixLight, SM1_WAIT2, SM1_WAIT3, SM1_WAIT4, SM1_WAIT5, SM1_WAIT6} SM1_STATE;
 void Tick_LoHi() { 
    switch(SM1_STATE) { 
       case SM1_SMStart:
          if (1) {
-            	SM1_STATE = SM1_nonePressed;
+            SM1_STATE = SM1_oneLight;
          }
          break;
-      case SM1_nonePressed:
-         if (((PINA & 0x01) == 0x01) && ((PINA & 0x02) != 0x02)) {
-            	SM1_STATE = SM1_firstPress;
-         }
-         else if (((PINA & 0x01) != 0x01) && ((PINA & 0x02) == 0x02)) {
-            	SM1_STATE = SM1_secondPress;
-         }
-         else if ((PINA & 0x03) == 0x03) {
-            	SM1_STATE = SM1_bothPress;
+      case SM1_oneLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_WAIT2;
          }
          else {
-            	SM1_STATE = SM1_nonePressed;
+            SM1_STATE = SM1_oneLight;
          }
          break;
-      case SM1_first:
-         if (((PINA & 0x01) == 0x01) && ((PINA & 0x02) != 0x02)) {
-            	SM1_STATE = SM1_first;
+      case SM1_twoLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_WAIT3;
          }
-         else if ((PINA & 0x02) == 0x02) {
-            	SM1_STATE = SM1_bothPress;
+	 else {
+	    SM1_STATE = SM1_twoLight;
+	 }
+         break;
+      case SM1_threeLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_WAIT4;
          }
          else {
-            	SM1_STATE = SM1_nonePressed;
+            SM1_STATE = SM1_threeLight;
          }
          break;
-      case SM1_second:
-        if (((PINA & 0x01) != 0x01) && ((PINA & 0x02) == 0x02)) {
-        	SM1_STATE = SM1_second;
-        }
-        else if ((PINA & 0x01) == 0x01) 
-	{
-            	SM1_STATE = SM1_bothPress;
-	}
-	else {
-		SM1_STATE = SM1_nonePressed;
-	}
-        break;
-	case SM1_secondPress:
-		SM1_STATE = SM1_second;
-		break;
-	case SM1_firstPress:
-		SM1_STATE = SM1_first;
-		break;
-      	case SM1_bothPress:
-	if ((PINA & 0x03) == 0x03)
-	{
-            	SM1_STATE = SM1_bothPress;
-	}
-	else
-	{
-		SM1_STATE = SM1_nonePressed;
-	}
-        break;
+      case SM1_fourLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_WAIT5;
+         }
+         else {
+            SM1_STATE = SM1_fourLight;
+         }
+         break;
+      case SM1_fiveLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_WAIT6;
+         }
+	 else {
+	    SM1_STATE = SM1_fiveLight;
+	 }
+         break;
+      case SM1_sixLight:
+         if ((~PINA & 0x01) == 0x01) {
+            SM1_STATE = SM1_oneLight;
+         }
+	 else {
+	    SM1_STATE = SM1_sixLight;
+	 }
+         break;
+      case SM1_WAIT2:
+	 if ((~PINA & 0x01) != 0x01) {
+	   SM1_STATE = SM1_twoLight;
+	 }
+      case SM1_WAIT3:
+	 if ((~PINA & 0x01) != 0x01) {
+	   SM1_STATE = SM1_threeLight;
+	 }
+      case SM1_WAIT4:
+	 if ((~PINA & 0x01) != 0x01) {
+	   SM1_STATE = SM1_fourLight;
+	 }
+      case SM1_WAIT5:
+	 if ((~PINA & 0x01) != 0x01) {
+	   SM1_STATE = SM1_fiveLight;
+	 }
+      case SM1_WAIT6:
+	 if ((~PINA & 0x01) != 0x01) {
+	   SM1_STATE = SM1_sixLight;
+	 }
+      default:
+         SM1_STATE = SM1_oneLight;
+         break;
    }
    switch(SM1_STATE) { 
       case SM1_SMStart:
-         PORTC = 0x07;
-         break;
-      case SM1_nonePressed:
          
          break;
-      case SM1_first:
-         
+      case SM1_oneLight:
+         PORTB = 0x01;
          break;
-      case SM1_firstPress:
-	 if (PORTC <= 0x08)
-	 {
-	 	PORTC++;
-	 }
-      case SM1_second:
-	
-	break;
-      case SM1_secondPress:
-	if (PORTC > 0x01)
-	 {
-		 PORTC--;
-	 }
+      case SM1_twoLight:
+         PORTB = 0x03;
+         break;
+      case SM1_threeLight:
+         PORTB = 0x07;
+         break;
+      case SM1_fourLight:
+         PORTB = 0x0F;
+         break;
+      case SM1_fiveLight:
+         PORTB = 0x1F;
+         break;
+      case SM1_sixLight:
+         PORTB = 0x3F;
+         break;
+      case SM1_WAIT2:
 	 break;
-      case SM1_bothPress:
-         PORTC = 0x00;
-         break;
+      case SM1_WAIT3:
+	 break;
+      case SM1_WAIT4:
+	 break;
+      case SM1_WAIT5:
+	 break;
+      case SM1_WAIT6:
+	 break;
    }
 }
-
-
-
-
-
-
-
-
 
