37b83e391768747d8116904a3e5cd2c0cdee9c9f
ethanphuong <ephuong@gmail.com>
2021-10-21 14:47:12 -0700

    Finished part 1

--- header/io.h
+++ header/io.h
@@ -6,6 +6,6 @@ void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
 void LCD_Cursor (unsigned char column);
 void LCD_WriteData (unsigned char Data);
-void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void LCD_DisplayString(unsigned char column , const unsigned char *string);
 void delay_ms(int miliSec);
 #endif
