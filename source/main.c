5e259f532e8162ba682daf86476a2aff5807185b
ethanphuong <ephuong@gmail.com>
2021-11-09 16:47:06 -0800

    Working

--- source/main.c
+++ source/main.c
@@ -9,43 +9,108 @@
  *	Demo Link: https://youtu.be/Fwu2PwC_K-w
  */
 #include <avr/io.h>
-#include <timer.h>
 #include <keypad.h>
-#include <io.h>
+#include <queue.h>
 #include <scheduler.h>
+#include <stack.h>
+#include <timer.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum SM_STATES {SM1_SMStart, SM_Change} SM_STATE;
+unsigned char x;
+void Tick_LoHi() {
+	x = GetKeypadKey();
+	switch (SM_STATE) {
+		case SM1_SMStart:
+		   SM_STATE = SM_Change;
+		   break;
+		case SM_Change:
+		   SM_STATE = SM_Change;
+		   break;
+		default:
+		   SM_STATE = SM1_SMStart;
+		   break;
+	}
+	switch (SM_STATE) {
+		case SM1_SMStart:
+		   break;
+		case SM_Change:
+		   switch(x) {
+		      case '\0':
+		         PORTB = 0x1F;
+		         break;
+		      case '0':
+		         PORTB = 0x00;
+		         break;
+		      case '1':
+		         PORTB = 0x01;
+		         break;
+		      case '2':
+		         PORTB = 0x02;
+		         break;
+		      case '3':
+		         PORTB = 0x03;
+		         break;
+		      case '4':
+		         PORTB = 0x04;
+		         break;
+		      case '5':
+		         PORTB = 0x05;
+		         break;
+		      case '6':
+		         PORTB = 0x06;
+		         break;
+		      case '7':
+		         PORTB = 0x07;
+		         break;
+		      case '8':
+		         PORTB = 0x08;
+		         break;
+		      case '9':
+		         PORTB = 0x09;
+		         break;
+		      case 'A':
+		         PORTB = 0x0A;
+		         break;
+		      case 'B':
+		         PORTB = 0x0B;
+		         break;
+		      case 'C':
+		         PORTB = 0x0C;
+		         break;
+		      case 'D':
+		         PORTB = 0x0D;
+		         break;
+		      case '*':
+		         PORTB = 0x0E;
+		         break;
+		      case '#':
+		         PORTB = 0x0F;
+		         break;
+	 	      default:
+		         PORTB = 0x1B;
+		         break;
+		   }
+	}
+}
+
+
 int main() {
-	DDRA = 0x00; PORTA = 0xFF:
+	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 
-	static _task task1, task2, task3, task4;
-	_task *tasks[] = { &task1, &task2, &task3, &task4 };
+	static task task1, task2, task3, task4;
+	task *tasks[] = { &task1, &task2, &task3, &task4 };
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-	const char start = -1;
+	const char start = 0;
 
 	task1.state = start;
 	task1.period = 50;
 	task1.elapsedTime = task1.period;
-	task1.TickFct = &pauseButtonToggleSMTick;
-
-	task2.state = start;
-	task2.period = 500;
-	task2.elapsedTime = task2.period;
-	task2.TickFct = &toggleLED0SMTick;
-	
-	task3.state = start;
-	task3.period = 1000;
-	task3.elapsedTime = task3.period;
-	task3.TickFct = &toggleLED1SMTick;
-
-	task4.state = start;
-	task4.period = 10;
-	task4.elapsedTime = task4.period;
-	task4.TickFct = &displaySMTick;
+	task1.TickFct = &Tick_LoHi;
 
 	TimerSet(50);
 	TimerOn();
@@ -53,7 +118,7 @@ int main() {
 	unsigned short i;
 	while (1) {
 		for ( i = 0; i < numTasks; i++) {
-			if ( tasks[i]->elapsedTime == tasks[i]->period) {
+			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
 				tasks[i]->elapsedTime = 0;
 			}
